bccb4496038a1513f6d743acb8010ac1a65b695c
tusharpandey <tushar.2april@gmail.com>
2015-09-26 17:13:07 +0530

    Application Setting Webserice is done

--- Constants.php
+++ Constants.php
@@ -2,4 +2,6 @@
     define("STATE_NAME", "UTTRAKHAND");
     define("PRIMARY_COLOR", "#00ffff");
     define("SECONDARY_COLOR", "#000000");
+    define("APPLICATION_LOGO", "#000000");
+    define("SPLASH_IMAGE", "#000000");
 ?>
--- splashFile.php
+++ splashFile.php
@@ -5,5 +5,14 @@
 	$msg = new Messages();
 	$res = new Results();
 	
-	
+	$res -> response = array ( 
+								"state_name" => STATE_NAME ,
+								"primary_color" => PRIMARY_COLOR , 
+								"secondary_color" => SECONDARY_COLOR ,
+								"splash_image" => SPLASH_IMAGE ,
+								"application_logo" => APPLICATION_LOGO 
+							 );
+							 
+	echo json_encode($res) ;
+							 
 ?>
